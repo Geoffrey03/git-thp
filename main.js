07f320b17a21c9438ca97c505453c792ac614a39
Votre nom ou pseudo <geoffrey.poirrier@essec.edu>
2019-01-02 13:31:14 +0100

    save version finale site with JS

---
+++ main.js
@@ -0,0 +1,2 @@
+setTimeout(function() {if (window.confirm("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please")) {
+  window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ",); } }, 10000);
